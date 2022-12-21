296767e6b9f4c1af8d67ba830801813f6abe225b
zsoon96 <pres0616@gmail.com>
2022-12-21 15:31:32 +0900

    Admin 예제 - firebase을 활용한 구글 로그인 인증

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,14 +1,27 @@
 import {Button} from "antd";
 import firebaseApp from '../net/firebaseApp'
-import { getAuth } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
-// firebase에서 제공하는 auth 기능 사용 (구글 로그인)
-const auth = getAuth(firebaseApp)
 
 export default function SignIn() {
     return (
         <div className=" flex justify-center items-center h-screen">
-            <Button>로그인</Button>
+            <Button onClick={async () => {
+                // firebase에서 제공하는 auth 기능 사용 (구글 로그인)
+                const auth = getAuth(firebaseApp)
+                // 인증 방식에 사용할 제공 업체 주입
+                const provider = new GoogleAuthProvider()
+                // 추가 정보 요청
+                provider.addScope('profile')
+                provider.addScope('email')
+                // 인증 진행
+                try {
+                    const result = await signInWithPopup(auth, provider)
+                    console.log(result)
+                } catch(err) {
+                    console.warn(err)
+                }
+            }}>로그인</Button>
         </div>
     )
 }
