e9cf0ef7f624f6bbc1719f894ad344dfed49b415
zsoon96 <pres0616@gmail.com>
2022-12-24 03:02:19 +0900

    Admin 예제 - firebase db를 활용한 게시글 작성

--- pages/portfolio/create.js
+++ pages/portfolio/create.js
@@ -1,12 +1,28 @@
 import BaseLayout from "../../components/containers/BaseLayout";
 import {Button, Form, Input} from 'antd'
+import firebaseApp from '../../net/firebaseApp';
+import { getFirestore, collection, addDoc } from 'firebase/firestore/lite'
+import {useRouter} from "next/router";
 
 export default function PortfolioForm() {
+    const router = useRouter()
     const [form] = Form.useForm()
+
     return (
         <BaseLayout>
-            <Form form={form} layout="vertical" onFinish={(values) => {
-                console.log(values)
+            <Form form={form} layout="vertical" onFinish={ (values) => {
+                // firebase db 객체 생성
+                const firebaseDb = getFirestore(firebaseApp)
+                // firebase db의 collection 객체 생성
+                const portfolios = collection(firebaseDb, 'portfolios')
+                // firebase db에 데이터 넣기
+                addDoc(portfolios,
+                    {...values,
+                        created_at: new Date(),
+                        updated_at: new Date()}
+                )
+                    .then(res => router.push('/portfolio'))
+                    .catch(console.warn)
             }}>
                 <Form.Item label='제목' required name="title">
                     <Input/>
