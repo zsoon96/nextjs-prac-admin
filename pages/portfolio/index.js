fa58e4391f71b9590bcb233d4a66ef423abce92c
zsoon96 <pres0616@gmail.com>
2022-12-24 17:15:08 +0900

    Admin 예제 - firebase db를 활용한 게시글 조회

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,12 +1,30 @@
 import BaseLayout from "../../components/containers/BaseLayout";
 import Link from "next/link";
 import {Button} from "antd";
+import {useEffect, useState} from "react";
+import firebaseApp from '../../net/firebaseApp';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
 
 export default function PortfolioList () {
+    const [portfolios, setPortfolios] = useState([])
+
+    useEffect(()=> {
+        const firebaseDb = getFirestore(firebaseApp)
+        const portfolios = collection( firebaseDb, 'portfolios')//.orderBy('created_at', 'desc')
+        getDocs(portfolios).then((getDataSnapshot) => {
+            // doc.data()를 통해 doc 데이터를 json으로 변환
+            setPortfolios(getDataSnapshot.docs.map(doc => ({id: doc.id, ...doc.data()})));
+        })
+    },[])
+
     return (
         <BaseLayout>
-            포트폴리오 리스트폼
-
+            <ul>
+                {portfolios.map(portfolio => {
+                    console.log(portfolio)
+                    return <li key={portfolio.id}>{portfolio.title}</li>
+                })}
+            </ul>
             <div className="flex flex-row justify-end">
                 <Link href="/portfolio/create">
                     <Button>추가</Button>
