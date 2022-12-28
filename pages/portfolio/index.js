6cbdf5ed62ba1b2682a6bdc8c7942898d0cbec14
zsoon96 <pres0616@gmail.com>
2022-12-28 09:13:55 +0900

    Admin 예제 - 게시글 목록 조회 시 썸네일 이미지/날짜 추가

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -3,25 +3,43 @@ import Link from "next/link";
 import {Button} from "antd";
 import {useEffect, useState} from "react";
 import firebaseApp from '../../net/firebaseApp';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
+import {getFirestore, collection, getDocs} from 'firebase/firestore/lite'
+import {DateTime} from "luxon";
 
-export default function PortfolioList () {
+function Items({portfolio}) {
+    return (
+        <li className='flex flex-row items-center py-2 border-b'>
+            <div className='w-16'>
+                <img src={portfolio.thumbnail} className='max-w-16 max-h-16' alt='썸네일 이미지'/>
+            </div>
+            <div className='flex-1 mx-2'>
+                {portfolio.title}
+            </div>
+            <div>
+                {/*{JSON.stringify(portfolio.created_at.seconds)}*/}
+                {/*{DateTime.fromSeconds(portfolio.created_at.seconds).toFormat('yyyy-LL-dd')}*/}
+            </div>
+        </li>
+    )
+}
+
+export default function PortfolioList() {
     const [portfolios, setPortfolios] = useState([])
 
-    useEffect(()=> {
+    useEffect(() => {
         const firebaseDb = getFirestore(firebaseApp)
-        const portfolios = collection( firebaseDb, 'portfolios')//.orderBy('created_at', 'desc')
+        const portfolios = collection(firebaseDb, 'portfolios')//.orderBy('created_at', 'desc')
         getDocs(portfolios).then((getDataSnapshot) => {
             // doc.data()를 통해 doc 데이터를 json으로 변환
             setPortfolios(getDataSnapshot.docs.map(doc => ({id: doc.id, ...doc.data()})));
         })
-    },[])
+    }, [])
 
     return (
         <BaseLayout>
             <ul>
                 {portfolios.map(portfolio => {
-                    return <li key={portfolio.id}>{portfolio.title}</li>
+                    return <li key={portfolio.id}><Items portfolio={portfolio}/></li>
                 })}
             </ul>
             <div className="flex flex-row justify-end">
