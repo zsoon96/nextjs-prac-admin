bc0c3c3066e3b32eb9126bfec7ab3c5cd60747e8
zsoon96 <pres0616@gmail.com>
2022-12-31 13:44:12 +0900

    Admin 예제 - firebase db를 활용한 게시글 삭제

--- components/views/PortfoliloForm.js
+++ components/views/PortfoliloForm.js
@@ -2,7 +2,7 @@ import {useRouter} from "next/router";
 import {Button, Form, Input} from "antd";
 import {useState} from "react";
 import BaseLayout from "../containers/BaseLayout";
-import {addDoc, collection, getFirestore, updateDoc, doc} from "firebase/firestore/lite";
+import {addDoc, collection, deleteDoc, doc, getFirestore, updateDoc} from "firebase/firestore/lite";
 import firebaseApp from "../../net/firebaseApp";
 import {getDownloadURL, getStorage, ref, uploadBytes} from "firebase/storage";
 import {DateTime} from "luxon";
@@ -14,39 +14,47 @@ export default function PortfolioForm({id, portfolio}) {
     const [form] = Form.useForm()
     const [thumbnail, setThumbnail] = useState(portfolio?.thumbnail)
 
+    const handleDelete = () => {
+        if ( !confirm ('삭제하시겠습니까?')) return
+        const firebaseDb = getFirestore(firebaseApp)
+        const docRef = doc(firebaseDb, 'portfolioList', id)
+        deleteDoc(docRef).then(() => router.push('/portfolio')).catch(console.warn)
+    }
+
     return (
         <BaseLayout>
-            <Form form={form} layout="vertical" initialValues={{ title: portfolio?.title, content: portfolio?.content }} onFinish={(values) => {
-                // firebase db 객체 생성
-                const firebaseDb = getFirestore(firebaseApp)
-                // firebase db의 collection 객체 생성
-                const portfolioList = collection(firebaseDb, 'portfolioList')
-                console.log(values)
+            <Form form={form} layout="vertical" initialValues={{title: portfolio?.title, content: portfolio?.content}}
+                  onFinish={(values) => {
+                      // firebase db 객체 생성
+                      const firebaseDb = getFirestore(firebaseApp)
+                      // firebase db의 collection 객체 생성
+                      const portfolioList = collection(firebaseDb, 'portfolioList')
+                      console.log(values)
 
-                // id 여부에 따른 수정/등록 분기 처리
-                if (!id) {
-                    // firebase db에 데이터 넣기
-                    addDoc(portfolioList,
-                        {
-                            ...values,
-                            thumbnail,
-                            created_at: new Date(),
-                            updated_at: new Date()
-                        })
-                        .then(() => router.push('/portfolio'))
-                        .catch(console.warn)
-                } else {
-                    const docRef = doc( firebaseDb, 'portfolioList', id)
-                    updateDoc( docRef, {
-                        ...values,
-                        thumbnail,
-                        updated_at: new Date(),
-                    })
-                        .then(() => router.push('/portfolio'))
-                        .catch(console.warn)
-                }
+                      // id 여부에 따른 수정/등록 분기 처리
+                      if (!id) {
+                          // firebase db에 데이터 넣기
+                          addDoc(portfolioList,
+                              {
+                                  ...values,
+                                  thumbnail,
+                                  created_at: new Date(),
+                                  updated_at: new Date()
+                              })
+                              .then(() => router.push('/portfolio'))
+                              .catch(console.warn)
+                      } else {
+                          const docRef = doc(firebaseDb, 'portfolioList', id)
+                          updateDoc(docRef, {
+                              ...values,
+                              thumbnail,
+                              updated_at: new Date(),
+                          })
+                              .then(() => router.push('/portfolio'))
+                              .catch(console.warn)
+                      }
 
-            }}>
+                  }}>
                 <Form.Item label='이미지' required>
                     {/* 파일 업로드 창이 안뜰경우에는 크롬 브라우저 업데이트 여부 확인 */}
                     <input type="file" onChange={async (e) => {
@@ -80,9 +88,18 @@ export default function PortfolioForm({id, portfolio}) {
                     <Input.TextArea/>
                 </Form.Item>
 
-                <Form.Item>
-                    <Button htmlType="submit">전송</Button>
-                </Form.Item>
+                <div className='flex flex-row justify-between'>
+                    <Form.Item>
+                        <Button type="primary" ghost htmlType="submit">전송</Button>
+                    </Form.Item>
+                    {/* id가 있을 경우에만 삭제 버튼 추가*/}
+                    {id && (
+                        <Form.Item>
+                            <Button type="primary" danger onClick={handleDelete}>삭제</Button>
+                        </Form.Item>
+                    )}
+                </div>
+
             </Form>
         </BaseLayout>
     )
